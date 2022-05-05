05e7df8f31534aab4cf7cc2528228956cf89434e
Piper McCorkle <piper@lutris.engineering>
2022-05-05 05:25:07 -0500

    Allow periods in URLs

--- config/routes.rb
+++ config/routes.rb
@@ -3,11 +3,11 @@
 Rails.application.routes.draw do
   root 'index#index'
   resources :part_attachments
-  resources :parts, param: :part_number
+  resources :parts, param: :part_number, constraints: { part_number: %r{[^/]+} }
 
   namespace :admin do
     get '/user', to: 'user#show'
-    resources :parts, param: :part_number do
+    resources :parts, param: :part_number, constraints: { part_number: %r{[^/]+} } do
       member do
         post :create_shopify
         post :destroy_shopify
