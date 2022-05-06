489d145c568dbaad0c22e2ba45c9723bca9c963f
Piper McCorkle <piper@lutris.engineering>
2022-05-06 12:52:28 -0500

    Change how attributes on buttons work

--- test/components/base_button_component_test.rb
+++ test/components/base_button_component_test.rb
@@ -4,12 +4,12 @@ require 'test_helper'
 
 class BaseButtonComponentTest < ViewComponent::TestCase
   def test_component_renders_content
-    render_inline(BaseButtonComponent.new(path: 'https://google.com')) { 'Hello, world!' }
+    render_inline(BaseButtonComponent.new(href: 'https://google.com')) { 'Hello, world!' }
     assert_text('Hello, world!')
   end
 
   def test_component_renders_path
-    render_inline(BaseButtonComponent.new(path: 'https://google.com')) { 'Hello, world!' }
+    render_inline(BaseButtonComponent.new(href: 'https://google.com')) { 'Hello, world!' }
     assert_selector :css, 'a[href="https://google.com"]', text: 'Hello, world!'
   end
 end
