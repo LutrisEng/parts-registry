a4ba382fd413da70be47353ecf494a7eab7ffba2
Piper McCorkle <piper@lutris.engineering>
2022-05-06 12:25:26 -0500

    Change Stimulus controller setup

--- app/components/base_button_component.rb
+++ app/components/base_button_component.rb
@@ -7,10 +7,11 @@ class BaseButtonComponent < ViewComponent::Base
     danger: 'focus:outline-none text-white bg-red-700 hover:bg-red-800 focus:ring-4 focus:ring-red-300 font-medium rounded-lg text-sm px-5 py-2.5 dark:bg-red-600 dark:hover:bg-red-700 dark:focus:ring-red-900'
   }.freeze
 
-  def initialize(path:, style: :secondary, target: '_self')
+  def initialize(path:, style: :secondary, target: '_self', additional_attrs: '')
     super
     @path = path
     @classes = STYLES[style]
     @target = target
+    @additional_attrs = additional_attrs
   end
 end
