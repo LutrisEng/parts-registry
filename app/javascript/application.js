90d2e91c62ae0707a20c81de9001af6a2aa2006d
Piper McCorkle <piper@lutris.engineering>
2022-05-06 15:15:50 -0500

    Gracefully handle Sentry failing to load

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -3,14 +3,16 @@ import "@hotwired/turbo-rails"
 import "controllers"
 import Rails from "@rails/ujs"
 
-Sentry.init({
-    dsn: "https://45dba99091084fb8b2a6859bd36e7981@o1155807.ingest.sentry.io/6377927",
-    integrations: [new Sentry.BrowserTracing({})],
+if (window.Sentry) {
+    window.Sentry.init({
+        dsn: "https://45dba99091084fb8b2a6859bd36e7981@o1155807.ingest.sentry.io/6377927",
+        integrations: [new Sentry.BrowserTracing({})],
 
-    // Set tracesSampleRate to 1.0 to capture 100%
-    // of transactions for performance monitoring.
-    // We recommend adjusting this value in production
-    tracesSampleRate: 1.0,
-})
+        // Set tracesSampleRate to 1.0 to capture 100%
+        // of transactions for performance monitoring.
+        // We recommend adjusting this value in production
+        tracesSampleRate: 1.0,
+    })
+}
 
 Rails.start()
